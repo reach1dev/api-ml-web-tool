189b93a02a561f76f0cc38aced7e4c90be2ea023
reach1dev <reach1dev@hotmail.com>
2020-05-28 22:20:38 +0300

    cors add

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ cors = CORS(app)
 input_data = []
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
+# @app.after_request
+# def after_request(response):
+#     response.headers.add('Access-Control-Allow-Origin', '*')
+#     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+#     return response
 
 
 def default(obj):
